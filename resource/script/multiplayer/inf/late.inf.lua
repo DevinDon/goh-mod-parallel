6408740fe5feb244705d3207304b77e2c645adbe
IInfinity <i.inf@outlook.com>
2024-03-17 05:14:24 +0800

    feat: 新增阵营飞升，新增国家无尽

---
+++ resource/script/multiplayer/inf/late.inf.lua
@@ -0,0 +1,16 @@
+Purchases["late.inf"] = {
+  {
+    Repeat = 0,
+    Units = {
+      -- 士兵
+      {priority = 1.0, type = {"Class3", "Infantry", "Team",}, unit = "soldier-engineer-team(inf)"},
+      -- 装甲车
+      -- 坦克
+      -- 学说
+      {priority = 1.0, type = {"Class2", "Doctrine", "Tier1", "Tank", "Light", "AT",}, unit = "tank-e-25"},
+      {priority = 1.0, type = {"Class3", "Doctrine", "Tier2", "Tank", "Medium",},      unit = "tank-e-50"},
+      {priority = 1.0, type = {"Class2", "Doctrine", "Tier3", "Tank", "Heavy",},       unit = "tank-e-75"},
+      {priority = 1.0, type = {"Class1", "Doctrine", "Tier3", "Tank", "Heavy",},       unit = "tank-e-100"},
+    }
+  }
+}
