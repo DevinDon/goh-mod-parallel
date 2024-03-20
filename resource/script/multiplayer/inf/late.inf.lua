ca73a057a7cddce1bffda0fdf48a1f9e381fac02
IInfinity <i.inf@outlook.com>
2024-03-20 23:47:31 +0800

    perf: 优化出兵逻辑

--- resource/script/multiplayer/inf/late.inf.lua
+++ resource/script/multiplayer/inf/late.inf.lua
@@ -3,20 +3,20 @@ Purchases["late.inf"] = {
     Repeat = 0,
     Units = {
       -- 排
-      {priority = 1.0, type = {"Class3", "Infantry", "Squad", "Team",}, unit = "soldier-antitank-platoon(inf)"},
-      {priority = 1.0, type = {"Class3", "Infantry", "Squad", "Team",}, unit = "soldier-engineer-platoon(inf)"},
-      {priority = 1.0, type = {"Class3", "Infantry", "Squad", "Team",}, unit = "soldier-mgun-platoon(inf)"},
+      {priority = 1.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-antitank-platoon(inf)"},
+      {priority = 1.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-engineer-platoon(inf)"},
+      {priority = 1.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-mgun-platoon(inf)"},
       -- 班
       {priority = 1.0, type = {"Class3", "Infantry", "Team",}, unit = "soldier-antitank-squad(inf)"},
       {priority = 1.0, type = {"Class3", "Infantry", "Team",}, unit = "soldier-engineer-squad(inf)"},
       {priority = 1.0, type = {"Class3", "Infantry", "Team",}, unit = "soldier-mgun-squad(inf)"},
       -- 装甲车
       -- 坦克
+      {priority = 1.0, type = {"Class1", "Tank", "Light", "AT",}, unit = "tank-e-25"},
+      {priority = 1.0, type = {"Class2", "Tank", "Medium",},      unit = "tank-e-50"},
+      {priority = 1.0, type = {"Class3", "Tank", "Heavy",},       unit = "tank-e-75"},
       -- 学说
-      {priority = 1.0, type = {"Class2", "Doctrine", "Tier1", "Tank", "Light", "AT",}, unit = "tank-e-25"},
-      {priority = 1.0, type = {"Class3", "Doctrine", "Tier2", "Tank", "Medium",},      unit = "tank-e-50"},
-      {priority = 1.0, type = {"Class2", "Doctrine", "Tier3", "Tank", "Heavy",},       unit = "tank-e-75"},
-      {priority = 1.0, type = {"Class1", "Doctrine", "Tier3", "Tank", "Heavy",},       unit = "tank-e-100"},
+      {priority = 1.0, type = {"Class3", "Doctrine", "Tier3", "Tank", "Heavy",},       unit = "tank-e-100"},
     }
   }
 }
