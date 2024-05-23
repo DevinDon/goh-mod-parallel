8a08892403cfca5a72fa084ef0d8da7bc4949ffd
IInfinity <i.inf@outlook.com>
2024-05-23 18:12:00 +0800

    feat: 移除 E 系列载具

--- resource/script/multiplayer/inf/late.inf.lua
+++ resource/script/multiplayer/inf/late.inf.lua
@@ -11,15 +11,15 @@ Purchases["late.inf"] = {
       {priority = 2.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-antitank-platoon(inf)"},
       {priority = 2.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-engineer-platoon(inf)"},
       {priority = 2.0, type = {"Class1", "Infantry", "Squad",}, unit = "soldier-mgun-platoon(inf)"},
-      -- 装甲车
-      -- 坦克
-      {priority = 1.5, type = {"Class", "Vehicle", "Unarmed", "Support", "Engineer",}, unit = "tank-ai-robot"},
-      {priority = 1.0, type = {"Class1", "Tank", "Light", "AT",}, unit = "tank-e-25"},
-      {priority = 2.0, type = {"Class2", "Tank", "Medium",},      unit = "tank-e-50"},
-      {priority = 2.0, type = {"Class3", "Tank", "Heavy",},       unit = "tank-e-75"},
-      -- 学说
-      -- 无尽重装军团
-      {priority = 1.0, type = {"Class3", "Doctrine", "Tier3", "Tank", "Heavy",}, unit = "tank-e-100"},
+      -- -- 装甲车
+      -- -- 坦克
+      -- {priority = 1.5, type = {"Class", "Vehicle", "Unarmed", "Support", "Engineer",}, unit = "tank-ai-robot"},
+      -- {priority = 1.0, type = {"Class1", "Tank", "Light", "AT",}, unit = "tank-e-25"},
+      -- {priority = 2.0, type = {"Class2", "Tank", "Medium",},      unit = "tank-e-50"},
+      -- {priority = 2.0, type = {"Class3", "Tank", "Heavy",},       unit = "tank-e-75"},
+      -- -- 学说
+      -- -- 无尽重装军团
+      -- {priority = 1.0, type = {"Class3", "Doctrine", "Tier3", "Tank", "Heavy",}, unit = "tank-e-100"},
     }
   }
 }
